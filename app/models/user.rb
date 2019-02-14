5529e0deb52e1f8b31de7ece6be4e7ed8ec64cfb
ajfunelas <31107247+ajfunelas@users.noreply.github.com>
2019-02-14 23:27:15 +0800

    init push

---
+++ app/models/user.rb
@@ -0,0 +1,118 @@
+class User < ApplicationRecord
+  # Associations
+  has_many :microposts, dependent: :destroy
+  # for following
+  has_many :active_relationships, class_name: 'Relationship',
+                                  foreign_key: 'follower_id',
+                                  dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  # for  followers
+  has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
+
+  attr_accessor :remember_token, :activation_token, :reset_token
+
+  # Before filters
+  before_create :create_activation_digest
+  before_save :downcase_email
+
+  # Validations
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                                    format: { with: VALID_EMAIL_REGEX },
+                                    :uniqueness => { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: {minimum: 6}, allow_nil: true
+
+  class << self
+    # Returs the hash digest of the given string
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+             BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    # Returns a random token
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+
+  # forget the user
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+  # Activate the account
+  def activate
+    update_columns(activated: true, activated_at: Time.zone.now)
+  end
+
+  # Send activation email
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+  # Set the password reset attributes
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
+  end
+
+  #Send reset password email
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  #Returns true is password reset has expired
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
+  # Defines a proto-feed
+  def feed
+    following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", following_ids: following_ids, user_id: id)
+  end
+
+  # Follow a user
+  def follow(other_user)
+    following << other_user
+  end
+
+  # Unfollow user
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  # Returns true if the current user is following other user
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
+  private
+
+  # convert email to downcase before submission
+  def downcase_email
+    email.downcase!
+  end
+
+  # create and assign the activation token digest
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,28 @@
+Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
+  root     'static_pages#home'
+  get      '/signup', to:'users#new'
+  get      '/help', to: 'static_pages#help'
+  get      '/home', to:'static_pages#home'
+  get      '/about', to:'static_pages#about'
+  get      '/contact', to:'static_pages#contact'
+  post     '/signup', to: 'users#create'
+  get      '/login', to: 'sessions#new'
+  post     '/login', to: 'sessions#create'
+  delete   '/logout', to: 'sessions#destroy'
+
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
+  resources :users
+  resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
+end
